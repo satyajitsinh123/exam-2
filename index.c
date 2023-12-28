c71124c4567fa149ddf6da7e922b2e06eaec68ef
satyajitsinh123 <satyajitsinh63@gamil.com>
2023-12-28 11:54:43 +0530

    hello

---
+++ index.c
@@ -0,0 +1,85 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+que 1    
+    
+  int c;
+   
+  printf("your number");
+  scanf("%d",&c);
+  (c%2==0);
+  printf("even number");
+  (c%2==1);
+  printf("odd number");
+
+que 2
+
+int month;
+printf("enter month");
+scanf("%d", &month);
+switch(month){
+    case 1:{
+        printf("jan");
+        break;
+    }
+    case 2:{
+        printf("feb");
+        break;
+    }
+    case 3:{
+        printf("mar");
+        break;
+    }
+    case 4:{
+        printf("apr");
+        break;
+    }
+    case 5:{
+        printf("may");
+        break;
+    }
+    case 6:{
+        printf("jun");
+        break;
+    }
+    case 7:{
+        printf("july");
+        break;
+    }
+    case 8:{
+        printf("ogt");
+        break;
+    }
+    case 9:{
+        printf("spe");
+        break;
+    }
+    case 10:{
+        printf("oct");
+        break;
+    }
+    case 11:{
+        printf("nav");
+        break;
+    }
+    case 12:{
+        printf("dec");
+        break;
+    }
+}
+
+que 3
+
+  int year;
+   
+  printf("enter year");
+  scanf("%d",&year);
+  (c%2==0);
+  printf("lip year");
+ 
+
+    return 0;
+}
